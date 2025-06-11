7a1211d0a9110ce57df7088f8f00c1bc0edcafe2
Syed Imran Ali <syed.imran.ali.edu@gmail.com>
2025-06-11 19:01:16 +0500

    Show Route 'READ', to display the details of each listings without image [at :  views\listings\show.ejs]

--- app.js
+++ app.js
@@ -35,6 +35,13 @@ app.get("/listings", async (req, res) => {
   res.render("listings/index.ejs", { allListings });
 });
 
+//Show Route
+app.get("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listings/show.ejs", { listing });
+});
+
 
 
 
