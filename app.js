73e61577a4ba2e140004944eb1b5a4f49032a7c1
Syed Imran Ali <syed.imran.ali.edu@gmail.com>
2025-06-12 22:11:31 +0500

    UPDATE : is achived by Edit Route (GET) and the Update Route (PUT), also install'method-override'

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
 const path = require("path");
+const methodOverride = require("method-override");
 
 
 //iii) DB connection
@@ -11,6 +12,7 @@ const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 async function main() {
   await mongoose.connect(MONGO_URL);
 }
+
 //iv) catching error in DB 
 main()
   .then(() => {
@@ -19,6 +21,11 @@ main()
   .catch((err) => {
     console.log(err);
   });
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 
 //ii)To get the output in the browser
@@ -55,23 +62,19 @@ app.post("/listings", async (req, res) => {
   res.redirect("/listings");
 });
 
+//Edit Route
+app.get("/listings/:id/edit", async (req, res) => {
+  let { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listings/edit.ejs", { listing });
+});
 
-
-// //Creating a route to TEST our first model at : 
-// // http://localhost:8080/testlisting
-// app.get("/testListing", async (req, res) => {
-//      let sampleListing = new Listing({
-//      title: "My New Villa",
-//      description: "By the beach",
-//      price: 1200,
-//      location: "Calangute, Goa",
-//      country: "India",
-//   });
-
-//    await sampleListing.save();
-//    console.log("sample was saved");
-//    res.send("successful testing");
-// });
+//Update Route
+app.put("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+  res.redirect(`/listings/${id}`);
+});
 
 // i)To make the server listening
 app.listen(8080, () => {
