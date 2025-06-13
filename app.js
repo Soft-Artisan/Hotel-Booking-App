fdcdd8dec701b24511f6817f6b2c3c576bdba1f2
Syed Imran Ali <syed.imran.ali.edu@gmail.com>
2025-06-13 12:14:30 +0500

    DELETE : Using delete Route, just add the add delete button with the form and write the delete route in app.js

--- app.js
+++ app.js
@@ -76,6 +76,14 @@ app.put("/listings/:id", async (req, res) => {
   res.redirect(`/listings/${id}`);
 });
 
+//Delete Route
+app.delete("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  let deletedListing = await Listing.findByIdAndDelete(id);
+  console.log(deletedListing);
+  res.redirect("/listings");
+});
+
 // i)To make the server listening
 app.listen(8080, () => {
   console.log("Our Server is Listening at Port 8080");
